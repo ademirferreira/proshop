c5d863f4d212885ecff30513ae36fffd766160c1
Ademir Ferreira <whoisademir@gmail.com>
2022-09-14 14:14:04 -0300

    Fix add payment method to localstorage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -24,7 +24,7 @@ const reducer = combineReducers({
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
-  ? JSON.parse(localStorage.getItem('cartItem'))
+  ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
@@ -35,10 +35,15 @@ const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
   ? JSON.parse(localStorage.getItem('shippingAddress'))
   : {}
 
+const paymentMethodFromStorage = localStorage.getItem('paymentMethod')
+  ? JSON.parse(localStorage.getItem('paymentMethod'))
+  : null
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
     shippingAddress: shippingAddressFromStorage,
+    paymentMethod: paymentMethodFromStorage,
   },
   userLogin: { userInfo: userInfoFromStorage },
 }
